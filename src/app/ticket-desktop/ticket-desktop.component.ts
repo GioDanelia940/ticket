1251fc00f536c1a41ec8511d250070f1a7d8d06f
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2023-03-27 03:24:55 +0400

    added bootstrap, material, font-awasome, ticket desktop and mobile components

---
+++ src/app/ticket-desktop/ticket-desktop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-desktop',
+  templateUrl: './ticket-desktop.component.html',
+  styleUrls: ['./ticket-desktop.component.scss']
+})
+export class TicketDesktopComponent {
+
+}
---
+++ src/app/ticket-mobile/ticket-mobile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-mobile',
+  templateUrl: './ticket-mobile.component.html',
+  styleUrls: ['./ticket-mobile.component.scss']
+})
+export class TicketMobileComponent {
+
+}
