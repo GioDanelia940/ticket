b6fe967d6a5e7987b611c362ed96f6cd805835dc
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2023-03-27 05:35:07 +0400

    added ticket-desktop component markup

--- src/app/ticket-desktop/ticket-desktop.component.ts
+++ src/app/ticket-desktop/ticket-desktop.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { faClockRotateLeft, faDove } from '@fortawesome/free-solid-svg-icons';
+import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-ticket-desktop',
   templateUrl: './ticket-desktop.component.html',
-  styleUrls: ['./ticket-desktop.component.scss']
+  styleUrls: ['./ticket-desktop.component.scss'],
 })
 export class TicketDesktopComponent {
-
+  faDove = faDove;
+  faPlane = faPlaneDeparture;
+  faClock = faClockRotateLeft;
 }
