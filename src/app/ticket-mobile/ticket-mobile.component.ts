779b8558e68656e0fa9fa1559a73212c12aa7712
GioDanelia940 <108083792+GioDanelia940@users.noreply.github.com>
2023-03-27 06:40:38 +0400

    added mobile component

--- src/app/ticket-mobile/ticket-mobile.component.ts
+++ src/app/ticket-mobile/ticket-mobile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { faRightLeft, faPlane } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-ticket-mobile',
   templateUrl: './ticket-mobile.component.html',
-  styleUrls: ['./ticket-mobile.component.scss']
+  styleUrls: ['./ticket-mobile.component.scss'],
 })
 export class TicketMobileComponent {
-
+  faRightLeft = faRightLeft;
+  faPlane = faPlane;
 }
